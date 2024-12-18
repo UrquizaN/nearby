52f529e2cdca33b955931992b1b46d7e61c67bed
UrquizaN <urquiza-nobrega@outlook.com>
2024-12-18 16:05:46 -0300

    feat: create categories component

---
+++ src/components/categories/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    height: 36,
+    zIndex: 1,
+    top: 64,
+  },
+  content: {
+    gap: 8,
+    paddingHorizontal: 24,
+  },
+});
