c8ad13ffc85fd7d5d1fd65b54eb364faade907bf
UrquizaN <urquiza-nobrega@outlook.com>
2024-12-19 11:23:07 -0300

    feat: implements map view on home screen

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,17 +2,28 @@ import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
 import { api } from "@/services/api";
+import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import { Alert, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-type MarketsPrps = PlaceProps & {
+type MarketsPrps = PlaceProps
 
+type LocationType = {
+  latitude: number;
+  longitude: number;
+}
+
+const spLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494
 }
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
   const [category, setCategory] = useState("")
   const [markets, setMarkets] = useState<MarketsPrps[]>([])
+  const [currentLocation, setCurrentLocation] = useState<LocationType>(spLocation)
 
   async function fetchCategories() {
     try {
@@ -37,7 +48,27 @@ export default function Home() {
     }
   }
 
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync()
+
+      if (granted) {
+        const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+
+        console.log(latitude, longitude)
+        setCurrentLocation({
+          latitude,
+          longitude
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Localização", "Erro ao carregar a localização")
+    }
+  }
+
   useEffect(() => {
+    getCurrentLocation()
     fetchCategories()
   }, [])
 
@@ -50,6 +81,21 @@ export default function Home() {
   return (
     <View style={{ flex: 1 }}>
       <Categories data={categories} onSelect={setCategory} selected={category} />
+      <MapView style={{ flex: 1 }} initialRegion={{
+        latitude: currentLocation.latitude,
+        longitude: currentLocation.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+        region={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}
+      >
+        <Marker identifier="current" image={require("@/assets/location.png")} coordinate={currentLocation} />
+      </MapView>
       <Places data={markets} />
     </View>
   )
